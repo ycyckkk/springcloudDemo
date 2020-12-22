4b4a958ef40f764a1db6ba7c278953c7c08bb2e5
于铖 <yuchengcida@sina.com>
2020-12-22 21:17:44 +0800

    代码提交，手动控制事务 transactionStatus=dataSourceTransactionManager.getTransaction(transactionDefinition)

--- client-producer/src/main/java/com/yc/service/impl/PaymentServiceImpl.java
+++ client-producer/src/main/java/com/yc/service/impl/PaymentServiceImpl.java
@@ -9,7 +9,10 @@ import com.yc.mapper.PaymentMapper;
 import com.yc.service.PaymentService;
 import org.redisson.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 
 import java.util.List;
 import java.util.Map;
@@ -29,52 +32,75 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private RedissonClient redissonClient;
 
+    @Autowired
+    DataSourceTransactionManager dataSourceTransactionManager;
+
+    @Autowired
+    TransactionDefinition transactionDefinition;
+
 //    public void addPayment() {
 //    }
 
     /**
-     * 修改支付信息，通过redis保证幂等性，防止高并发场景下重复请求和消息重复消费的情况
-     * 非高并发场景可以使用分布式锁或者数据库乐观锁or死锁进行控制
+     * 接口幂等解决思路：
+     * 场景：新增接口（可能会生成重复数据），修改接口（可能会生成错误数据）
+     * 原因：高并发场景下重复请求和消息重复消费的情况
+     * 思路：
+     * 1）通过redis保证幂等性，操作之前查询一下
+     * <p>
+     * 2）非高并发场景可以使用分布式锁或者数据库乐观锁or死锁进行控制
+     * <p>
+     * 3）通过唯一约束保证
+     * <p>
+     * 4）消息记录表
      *
      * @param payment
      */
     public void updatePayment(Payment payment) {
-        long time = System.currentTimeMillis();
-        String key = payment.getPaymentId() + ":" + time;
+        TransactionStatus transactionStatus = dataSourceTransactionManager.getTransaction(transactionDefinition);
+
+        dataSourceTransactionManager.commit(transactionStatus);
+//        long time = System.currentTimeMillis();
+        //业务id作为key值
+        String key = payment.getPaymentId();
         RBuckets buckets = redissonClient.getBuckets();
         Map<String, String> map = buckets.get(key);
         System.out.println("map1=" + JSON.toJSONString(map));
-        redissonClient.getBucket(key).set(1, 60, TimeUnit.SECONDS);
-        Map<String, String> map1 = buckets.get(key);
-        System.out.println("map2=" + JSON.toJSONString(map1));
+        if (map != null && !map.isEmpty()) {
+            return;
+        }
+        //设置缓存
+        redissonClient.getBucket(key).set(1, 5, TimeUnit.SECONDS);
+        Map<String, String> map2 = buckets.get(key);
+        System.out.println("map2=" + JSON.toJSONString(map2));
+
+        //更新数据
         paymentMapper.update(payment, new UpdateWrapper<Payment>().eq("payment_id", payment.getPaymentId()));
+        dataSourceTransactionManager.rollback(transactionStatus);
+
     }
 
-    //思路：先查缓存，在查布隆过滤器，在查本地缓存，在查数据库
+    //缓存穿透解决思路：先查缓存，在查布隆过滤器，在查本地缓存，在查数据库
     public String getPayment(String id) {
         //先查缓存
 //        RBucket<String> bucket = redissonClient.getBucket("payment");
         RMap map = redissonClient.getMap("payment");
         if (map != null) {
             String str = (String) map.get(id);
-            if (null!= str && !str.isEmpty()) {
+            if (null != str && !str.isEmpty()) {
                 return str;
             }
         }
-
         //为空就查布隆过滤器，不存在就直接返回
         RBloomFilter<String> bloomFilter = redissonClient.getBloomFilter("sample");
         if (!bloomFilter.contains(id)) {
             return "";
         }
-
         //存在则查询数据库
         Wrapper<Payment> wrapper = new QueryWrapper<Payment>().eq("payment_id", id);
         String result = JSON.toJSONString(paymentMapper.selectOne(wrapper));
-        //放入缓存中
+        //异步放入缓存中
         map.putAsync(id, result);
-
-
 
         return result;
     }
